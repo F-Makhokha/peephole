366b8a9ce98dc721731bc1a8532b8f1dce41a698
natmg <nathangoldman1@gmail.com>
2018-02-22 05:12:24 +0000

    Adding cameracode

--- cameraRead.py
+++ cameraRead.py
@@ -6,11 +6,12 @@ cap = cv2.VideoCapture(0) #captures from webcam
 
 while True:
     ret, frame = cap.read()
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('live-color',frame)
+    cv2.imshow('gray-scale',gray)
 
-    cv2.imshow('live color',frame)
-
-    if vc2.waitKey(1) and oxFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
