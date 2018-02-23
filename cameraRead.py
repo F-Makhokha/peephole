56565befa13cd1bf818d5ed820e65aa352a393f9
hocally <henry.ocallaghan@gmail.com>
2018-02-23 09:16:11 -0600

    Changed filenames

--- cameraRead.py
+++ cameraRead.py
@@ -1,21 +1,25 @@
 import numpy as np
-import cv2 as cv
+import cv2
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-while True:
+while(True):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if frame.type() == 1:
-        gray = frame
-    elif frame.type() == 3:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    elif frame.type() == 4:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('raw', frame)
-    cv.imshow('grayscale', gray)
 
-    if cv.waitKey(1) & 0xff == ord('q'):
+    try:
+        height, width = frame.shape[:2]
+        thumbnail = cv2.resize(frame, (int(width/1), int(height/1)), interpolation = cv2.INTER_AREA)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(thumbnail, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+
+    except AttributeError:
+        print("no vid")
+    if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
+# When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()                      
