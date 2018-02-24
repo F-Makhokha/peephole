a4bdc2a88470c8e03305a8fdbfb27b1e34c6004d
hocally <henry.ocallaghan@gmail.com>
2018-02-24 00:04:49 -0600

    Added a dot at the center of the bounding box

--- contourBounding.py
+++ contourBounding.py
@@ -1,7 +1,12 @@
 import numpy as np
 import cv2
 
-
+xPos = 0
+yPos = 0
+x = 0
+y = 0
+w = 0
+h = 0
 cap = cv2.VideoCapture(0)
 fps = 0
 while(True):
@@ -11,6 +16,8 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     height, width = frame.shape[:2]
+    xPos = width/2
+    yPos = height/2
     frame = cv2.resize(frame, (int(width/1), int(height/1)), interpolation = cv2.INTER_AREA)
 	
     # Our operations on the frame come here
@@ -24,9 +31,12 @@ while(True):
 	cv2.drawContours(frame, contours, -1, (255, 255, 0), 1)
 	c = max(contours, key = cv2.contourArea)
 	x, y, w, h = cv2.boundingRect(c)
+	xPos = x - width/2 + w/2
+	yPos = y - height/2 + h/2
 	cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
     #Displays frame
-
+    print(xPos, yPos)
+    cv2.circle(frame, (x+w/2, y+h/2), (min([w, h])/8), (100, 100, 100), -1)
     cv2.imshow('peephole', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
